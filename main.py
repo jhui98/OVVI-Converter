280765ca2832b08e6c7f279b225a103b193a26b8
jhui98 <jacob.hui@live.com>
2022-07-22 20:49:31 -0500

    process and gui working, need to generate save

--- main.py
+++ main.py
@@ -4,56 +4,58 @@ from methods import get_departments, item_department_dict, initialItemIstance, O
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 
-print(logo)
-# import file path from clover
-clover_folder = input(r"Input the folder path that you are using: ") 
-clover_file = input(r"Input file name: ") + ".xlsx"
-# print(clover_folder, clover_file)
+run = "y"
+while run != "n":
+    print(logo)
 
-# test file
-# clover_folder = r"C:\Users\Ovvi\OneDrive\Desktop\Automation\Clover" 
-# clover_file = r"\testclover.xlsx" 
-clover_file = "testclover.xlsx" 
+    task = input("""
+    A: Clover Conversion
+    B: Double Check My Work (Coming soon)
+    C: Barcode Leading Zero Fix (Coming soon)
 
-# sanity check
-clover_path = clover_folder + clover_file
-print(clover_folder)
-print(clover_file)
-print(clover_path)
+    Please choose an operation: """)
 
-# load worksheet
-clover_wb = load_workbook(clover_path)
-clover_ws = clover_wb.active
-print(clover_wb.sheetnames)
+    if task == "A" or "a": # CLover to OVVI format change
+        clover_path = input("\nPlease drag and drop the file, then press Enter: ")
+        clover_path = clover_path[3:-1]
+        # print(clover_path) # sanity check
+        print("\nProcessing, please wait...\n")
+        # import file path from clover
+        # clover_folder = input(r"Input the folder path that you are using: ") 
+        # clover_file = input(r"Input file name: ") + ".xlsx"
+        # print(clover_folder, clover_file)
 
-# get departments
-departments = get_departments(clover_wb)
+        # load worksheet
+        clover_wb = load_workbook(clover_path)
+        clover_ws = clover_wb.active
+        # print(clover_wb.sheetnames) # sanity check
 
-# create dictionary of items with assigned department
-items_dict = item_department_dict(clover_wb, departments)
+        departments = get_departments(clover_wb) # get departments
 
-items = initialItemIstance(clover_wb, departments)
+        items_dict = item_department_dict(clover_wb, departments) # create dictionary of items with assigned department
+        items = initialItemIstance(clover_wb, departments)
 
-# double checking
-# for item in items:
-#     if item.itemName == '12CARLO ROSSI BURGUNDY 4L':
-#         print(item.itemName)
-#         print(item.itemDepartment)
-#         print(item.itemSellPrice)
-#         print(item.itemBarcode)
-#         print(item.itemCost)
-#         print(item.itemStock)
-# print("-----")
-# for item in items:
-#     if item.itemName == 'Goldwater Marlborough Char.':
-#         print(item.itemName)
-#         print(item.itemDepartment)
-#         print(item.itemSellPrice)
-#         print(item.itemBarcode)
-#         print(item.itemCost)
-#         print(item.itemStock)
-
-# store in new sheet
-# save
-print()
-print("Finished")
+        # double checking
+        # for item in items:
+        #     if item.itemName == '12CARLO ROSSI BURGUNDY 4L':
+        #         print(item.itemName)
+        #         print(item.itemDepartment)
+        #         print(item.itemSellPrice)
+        #         print(item.itemBarcode)
+        #         print(item.itemCost)
+        #         print(item.itemStock)
+        # print("-----")
+        # for item in items:
+        #     if item.itemName == 'Goldwater Marlborough Char.':
+        #         print(item.itemName)
+        #         print(item.itemDepartment)
+        #         print(item.itemSellPrice)
+        #         print(item.itemBarcode)
+        #         print(item.itemCost)
+        #         print(item.itemStock)
+        
+        # TODO: store in new sheet
+        # TODO: save
+    
+    print("Finished! Please check your downloads folder for the updated file.\n")
+    run = input("Would you like to process another operation? y or n: ")
--- methods.py
+++ methods.py
@@ -6,9 +6,7 @@ logo = """
  | |  | |_   ____   ___  | |     ___  _ ____   _____ _ __| |_ ___ _ __ 
  | |  | \ \ / /\ \ / / | | |    / _ \| '_ \ \ / / _ \ '__| __/ _ \ '__|
  | |__| |\ V /  \ V /| | | |___| (_) | | | \ V /  __/ |  | ||  __/ |   
-  \____/  \_/    \_/ |_|  \_____\___/|_| |_|\_/ \___|_|   \__\___|_|   
-                                                                       
-                                                                       
+  \____/  \_/    \_/ |_|  \_____\___/|_| |_|\_/ \___|_|   \__\___|_|    -Jacob Hui                                                                      
 """
 
 from openpyxl import Workbook, load_workbook
@@ -71,7 +69,6 @@ def init_OVVI(folder):
     ws1 = wb.active
     ws1.title = "Item-PLU" 
     ws2 = wb.create_sheet(title="ModifierGroups")   
-    
     
     wb.save(filename = dest_filename)
 
